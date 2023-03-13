e4631deab05a9a1eeef250dbfa2edd1dcb9f0400
Omer Gencoglu <omersaitgencoglu@gmail.com>
2023-03-13 18:56:21 -0400

    Add userform component

---
+++ users/src/UserForm.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function UserForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <button>Add User</button>
+    </form>
+  );
+}
