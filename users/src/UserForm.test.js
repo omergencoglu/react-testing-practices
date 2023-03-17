7807d00e28aefd08176aeed88055113c3210ebdd
Omer Gencoglu <omersaitgencoglu@gmail.com>
2023-03-17 18:42:54 -0400

    Add form submit test

--- users/src/UserForm.test.js
+++ users/src/UserForm.test.js
@@ -16,14 +16,33 @@ test("it shows two inputs and a button", () => {
 });
 
 test("it calls onUserAdd when the form is submitted", () => {
+  //not the best implementation;
+  const arglist = [];
+  const callback = (...args) => {
+    arglist.push(args);
+  };
+
   // Try to render component
-  render(<UserForm />);
+  render(<UserForm onUserAdd={callback} />);
 
   // Find the two inputs
-  const inputs = screen.getAllByRole("textbox");
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+
   // Simulate typing in a name
+  user.click(nameInput);
+  user.keyboard("omer");
+
   // Simulate typing in an email
+  user.click(emailInput);
+  user.keyboard("omer@omer.com");
+
   // Find the button
+  const button = screen.getByRole("button");
+
   // Simulate clicking the button
+  user.click(button);
+
   // Assertion to make sure "onUserAdd" gets called with email/name
+  expect(arglist).toHaveLength(1);
+  expect(arglist[0][0]).toEqual({ name: "omer", email: "omer@omer.com" });
 });
