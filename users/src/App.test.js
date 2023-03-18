a8c80d495e6d275e4b89ddef1add0ccb0d1b2b29
Omer Gencoglu <omersaitgencoglu@gmail.com>
2023-03-18 18:48:18 -0400

    Add test for app.js

--- users/src/App.test.js
+++ users/src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("can receive a new user and show it on a list", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const nameInput = screen.getByRole("textbox", {
+    name: /name/i,
+  });
+  const emailInput = screen.getByRole("textbox", {
+    name: /email/i,
+  });
+  const button = screen.getByRole("button");
+
+  user.click(nameInput);
+  user.keyboard("omer");
+  user.click(emailInput);
+  user.keyboard("omer@omer.com");
+
+  user.click(button);
+
+  const name = await screen.findByRole("cell", { name: "omer" });
+  const email = await screen.findByRole("cell", { name: "omer@omer.com" });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
 });
