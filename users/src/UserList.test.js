bd90d9f1ed6def05cea273c8a9295b53bbcfd7b1
Omer Gencoglu <omersaitgencoglu@gmail.com>
2023-03-18 18:04:03 -0400

    Update userlist test

--- users/src/UserList.test.js
+++ users/src/UserList.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import UserList from "./UserList";
 
 test("render one row per user", () => {
@@ -7,11 +7,14 @@ test("render one row per user", () => {
     { name: "omer", email: "omer@omer.com" },
     { name: "nihal", email: "nihal@nihal.com" },
   ];
-  render(<UserList users={users} />);
+  const { container } = render(<UserList users={users} />);
 
   // Find all the rows in the table
+  //eslint-disable-next-line
+  const rows = container.querySelectorAll("tbody tr");
 
   // Assertion: correct number of rows in the table
+  expect(rows).toHaveLength(2);
 });
 
 test("render the email and name of each user", () => {});
