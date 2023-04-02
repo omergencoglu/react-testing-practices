f0ce316daff9c7ec61a5ef13e201c75fd91b52ee
Omer Gencoglu <omersaitgencoglu@gmail.com>
2023-04-02 12:12:27 -0400

    Scope tests

--- codesplain/src/components/auth/AuthButtons.test.js
+++ codesplain/src/components/auth/AuthButtons.test.js
@@ -1,12 +1,42 @@
-import { screen, render } from "@testing-library/react";
-import { MemoryRouter } from "react-router";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import { createServer } from "../../test/server";
 import AuthButtons from "./AuthButtons";
 
-test("when user is not signed in, sign in and sign up are visible", async () => {});
+describe("when user is not signed in", () => {
+  // createServer() ---> GET '/api/user' ---> { user: null }
+  createServer([
+    {
+      path: "/api/user",
+      res: () => {
+        return { user: null };
+      },
+    },
+  ]);
 
-test("when user is not signed in, sign out is not visible", async () => {});
+  test("when user is not signed in, sign in and sign up are visible", async () => {
+    console.log("Test 1");
+  });
+  test("when user is not signed in, sign out is not visible", async () => {
+    console.log("Test 2");
+  });
+});
 
-test("when user is signed in, sign in and sign up are not visible", async () => {});
+describe("when user is signed in", () => {
+  // createServer() ---> GET '/api/user' ---> { user: { id: 3, email: 'asdf@a.com' }}
+  createServer([
+    {
+      path: "/api/user",
+      res: () => {
+        return { user: { id: 3, email: "asdf@asdf.com" } };
+      },
+    },
+  ]);
 
-test("when user is signed in, sign out is visible", async () => {});
+  test("when user is signed in, sign in and sign up are not visible", async () => {
+    console.log("Test 3");
+  });
+  test("when user is signed in, sign out is visible", async () => {
+    console.log("Test 4");
+  });
+});
