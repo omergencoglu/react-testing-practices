447c7424127a49dac1b6991374fec971219bcf55
Omer Gencoglu <omersaitgencoglu@gmail.com>
2023-04-02 12:27:37 -0400

    Complete the first tests

--- codesplain/src/components/auth/AuthButtons.test.js
+++ codesplain/src/components/auth/AuthButtons.test.js
@@ -3,12 +3,14 @@ import { MemoryRouter } from "react-router-dom";
 import { createServer } from "../../test/server";
 import AuthButtons from "./AuthButtons";
 
-function renderComponent() {
-  return render(
+async function renderComponent() {
+  render(
     <MemoryRouter>
       <AuthButtons />
     </MemoryRouter>
   );
+
+  await screen.findAllByRole("link");
 }
 
 describe("when user is not signed in", () => {
@@ -23,30 +25,40 @@ describe("when user is not signed in", () => {
   ]);
 
   test("sign in and sign up are visible", async () => {
-    renderComponent();
-    await screen.findAllByRole("link");
+    await renderComponent();
+
+    const signInButton = screen.getByRole("link", { name: /sign in/i });
+    const signUpButton = screen.getByRole("link", { name: /sign up/i });
+
+    expect(signInButton).toBeInTheDocument();
+    expect(signInButton).toHaveAttribute("href", "/signin");
+    expect(signUpButton).toBeInTheDocument();
+    expect(signUpButton).toHaveAttribute("href", "/signup");
   });
   test("sign out is not visible", async () => {
-    renderComponent();
-    await screen.findAllByRole("link");
+    await renderComponent();
+
+    const signOutButton = screen.queryByRole("link", { name: /sign out/i });
+
+    expect(signOutButton).not.toBeInTheDocument();
   });
 });
 
-describe("when user is signed in", () => {
-  // createServer() ---> GET '/api/user' ---> { user: { id: 3, email: 'asdf@a.com' }}
-  createServer([
-    {
-      path: "/api/user",
-      res: () => {
-        return { user: { id: 3, email: "asdf@asdf.com" } };
-      },
-    },
-  ]);
+// describe("when user is signed in", () => {
+//   // createServer() ---> GET '/api/user' ---> { user: { id: 3, email: 'asdf@a.com' }}
+//   createServer([
+//     {
+//       path: "/api/user",
+//       res: () => {
+//         return { user: { id: 3, email: "asdf@asdf.com" } };
+//       },
+//     },
+//   ]);
 
-  test("sign in and sign up are not visible", async () => {
-    renderComponent();
-  });
-  test("sign out is visible", async () => {
-    renderComponent();
-  });
-});
+//   test("sign in and sign up are not visible", async () => {
+//     await renderComponent();
+//   });
+//   test("sign out is visible", async () => {
+//     await renderComponent();
+//   });
+// });
